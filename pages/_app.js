46305d6d7f7b0e16ce620139dcefc6b139c001c0
jaksonlima <jakson@smartbr.com>
2021-03-12 00:01:04 -0300

    Config styled-components

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,30 @@
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+
 import { wrapper } from "../src/redux/store";
 
-import "../styles/globals.css";
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
+
+const theme = {
+  colors: {
+    primary: "#0070f3",
+  },
+};
 
 function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default wrapper.withRedux(App);
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
 import Head from "next/head";
-import { connect, useDispatch } from "react-redux";
+import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
+import styled from "styled-components";
 
 import { Creators } from "../src/redux/reducers/home";
+
+const H2 = styled.div`
+  color: ${({ theme }) => theme.colors.primary};
+`;
 
 function Home({ image, carouselRequest }) {
   carouselRequest({ dispatch: "saga home" });
@@ -13,7 +18,7 @@ function Home({ image, carouselRequest }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>aa</h1>
+      <H2>aa</H2>
     </>
   );
 }
