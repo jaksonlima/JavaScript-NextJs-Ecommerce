46927643df5293669c69256c40547fc233d941b5
jaksonlima <jakson@smartbr.com>
2021-04-02 13:54:06 -0300

    Adicionado favorito

--- src/components/CardProduct/CardProduct.js
+++ src/components/CardProduct/CardProduct.js
@@ -1,8 +1,9 @@
 import Image from "next/image";
 
-import { CardContent, CardMedia, Typography, Grid, Box } from "@material-ui/core";
+import { CardContent, CardMedia, Typography, Grid, Box, CardActions } from "@material-ui/core";
+import { FavoriteBorder as FavoriteIcon } from "@material-ui/icons";
 
-import { CardMUI } from "./styles";
+import { CardMUI, FavoriteBorderIconMUI } from "./styles";
 
 import { moeda } from "../../utils/formater";
 
@@ -11,16 +12,27 @@ export default function MediaCard({ product }) {
   return (
     <Box m={1}>
       <CardMUI>
+        <Grid container justifyContent="flex-start">
+          <Grid item>
+            <CardActions>
+              <FavoriteBorderIconMUI />
+            </CardActions>
+          </Grid>
+        </Grid>
         <Grid container direction="row" justify="center" alignItems="center">
-          <CardMedia children={<Image src={product.image} width={145} height={145} />} />
-          <CardContent>
-            <Typography gutterBottom variant="body2" component="h5">
-              {product.name}
-            </Typography>
-            <Typography variant="h6" component="span">
-              {moeda(product.price)}
-            </Typography>
-          </CardContent>
+          <Grid item>
+            <CardMedia children={<Image src={product.image} width={145} height={145} />} />
+          </Grid>
+          <Grid item>
+            <CardContent>
+              <Typography gutterBottom variant="body2" component="h5">
+                {product.name}
+              </Typography>
+              <Typography variant="h6" component="span">
+                {moeda(product.price)}
+              </Typography>
+            </CardContent>
+          </Grid>
         </Grid>
       </CardMUI>
     </Box>
--- src/components/CardProduct/styles.js
+++ src/components/CardProduct/styles.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 import { Card } from "@material-ui/core";
+import { FavoriteBorder } from "@material-ui/icons";
 
 export const CardMUI = styled(Card)`
   width: 252px;
@@ -17,5 +18,19 @@ export const CardMUI = styled(Card)`
     transform: translateY(-8px);
 
     box-shadow: rgba(0 0 0 / 40%) 0px 3px 6px !important;
+  }
+`;
+
+export const FavoriteBorderIconMUI = styled(FavoriteBorder)`
+  color: #5b898a;
+
+  transition: all 0.2s ease-out !important;
+
+  &:hover,
+  &:focus,
+  &:active {
+    color: red;
+    -webkit-transform: translateY(-5px);
+    transform: translateY(-5px);
   }
 `;
